583afd4fddec596df378b640ebbed73c1e50cf4f
Alexey <slow.net@mail.ru>
2022-11-26 18:17:44 +0300

    homework number nine

---
+++ Task1.0/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите метод, 
+// который выведет все натуральные числа
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+Console.WriteLine("Введите число N");
+bool isParsedN= int.TryParse(Console.ReadLine(), out int n);
+if(!isParsedN || n<=0)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+AllNumber(n);
+void AllNumber(int n, int m=1)
+{
+    if(n>=m)
+    {
+        Console.Write($"{n},");
+        AllNumber(n-1);
+    }
+}
+
---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите метод,
+// который найдёт сумму натуральных элементов
+// в промежутке от M до N.Через рекурсию!
+Console.WriteLine("Введите число M");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите число N");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+if (!isParsedM || !isParsedN || m < 0 || n < 0 || m > n)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+int sumOfNumbersBetween = SumOfNumbersBetween(m, n);
+Console.WriteLine(sumOfNumbersBetween);
+int SumOfNumbersBetween(int m, int n, int sum = 0)
+{
+    if (m == n)
+    {
+        return n;
+    }
+    return m + SumOfNumbersBetween(m + 1, n);
+}
